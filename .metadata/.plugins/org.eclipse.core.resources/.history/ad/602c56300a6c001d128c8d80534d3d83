58549a683e1705c62d72d9212e7342fa53372ef0
darshankalathiya9 <darshankalathiya963@gmail.com>
2022-11-25 17:50:09 +0530

    Done

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ad/602c56300a6c001d128c8d80534d3d83
@@ -0,0 +1,6 @@
+
+public class System_Time {
+public static void main(String[] args) {
+	System.out.format("Current Date Time : %tc%n\n", System.currentTimeMillis());
+}
+}
---
+++ COREJAVA_assignments/src/Cconcatenate.java
@@ -0,0 +1,11 @@
+
+public class Cconcatenate {
+public static void main(String[] args) {
+	String s = "Tops ";
+	String s1= "Technologies";
+	
+	System.out.println("String 1 : "+s);
+	System.out.println("String 2 : "+s1);
+	System.out.println("String Concatenate : "+s.concat(s1));
+}
+}
